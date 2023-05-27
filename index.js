1809a17943c75b0d105561fa4e9b4cb9a163ed04
Krishnanand K B <krishnanandavittam@gmail.com>
2023-05-27 12:00:32 +0530

    added event to start button

--- index.js
+++ index.js
@@ -11,3 +11,32 @@ let intervalId;
 let hrs = 0;
 let mins = 0;
 let secs = 0;
+
+startBtn.addEventListener("click",() =>{
+    if(paused){
+        paused = false;
+        startTime = Date.now() - elapsedTime;
+        intervalId = setInterval(updateTime,100)
+    }
+});
+resetBtn.addEventListener();
+pauseBtn.addEventListener();
+
+function updateTime(){
+
+    elapsedTime = Date.now()- startTime;
+
+    secs = Math.floor((elapsedTime/1000)% 60);
+    mins = Math.floor((elapsedTime/(1000* 60))% 60);
+    hrs = Math.floor((elapsedTime/(1000* 60*60))% 60); 
+
+    secs = pad(secs);
+    mins = pad(mins);
+    hrs = pad (hrs);
+     
+    timeDisplay.textContent = `${hrs}:${mins}:${secs}`;
+
+    function pad(unit){
+        return (("0") + unit).length > 2?unit : "0" +unit;
+     }
+}
